73178d9c91f0ae5b43e01edc79a332fa95279116
Ain <ain.salula@gmail.com>
2017-01-29 02:51:28 +0200

    using fetch to load data from server

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1, "firstName": "Bob", lastName: "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "Tammy", lastName: "Norton", "email": "tnorton@gmail.com"},
+    {"id": 3, "firstName": "Tina", lastName: "Lee", "email": "tina@gmail.com"},
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
